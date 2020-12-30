76c6017754eab05b75ae601f2dd93895d54a7c70
caiosbl <caio.lira@ccc.ufcg.edu.br>
2020-12-30 00:09:49 -0300

    Remove unused Apis

--- src/Services/Api.js
+++ src/Services/Api.js
@@ -1,23 +1,5 @@
 import Axios from 'axios';
 
-export const WorldApi = Axios.create({
-  baseURL: `https://api.covid19api.com/summary`
-});
-
-export const Api = Axios.create({
-  baseURL: 'https://xx9p7hp1p7.execute-api.us-east-1.amazonaws.com/prod/PortalMapa',
-  headers: {  'x-parse-application-id': 'unAFkcaNDeXajurGB7LChj8SgQYS2ptm' },
-  timeout: 1000
-});
-
-export const BrazilApi = Axios.create({
-  baseURL: `https://covid19-brazil-api.now.sh/api/report/v1`,
-});
-
-export const TimelineApi = Axios.create({
-  baseURL: `https://pomber.github.io/covid19/timeseries.json`
-});
-
 export const GeoJson = Axios.create({
   baseURL: `./br-all.geo.json`,
 });
@@ -28,6 +10,4 @@ export const GeoJsonWorld = Axios.create({
 
 export const CoronavairusApi = Axios.create({
   baseURL: `https://coronavairus.herokuapp.com/`,
-  //baseURL: `http://10.0.0.121:8089/`,
 });
-
